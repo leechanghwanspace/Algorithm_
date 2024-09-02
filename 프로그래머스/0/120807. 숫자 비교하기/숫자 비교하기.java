c4a5bff021b8d25baa50070728cb70ce575befdd
HWAN <spacecehh@naver.com>
2024-09-02 21:12:04 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.06 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,15 +1,13 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
         
-        if(num1 == num2){
-            answer = 1;
-        }
-        else {
-            answer = -1;
-        }
-        return answer;
+        if (num1 == num2) {
+            int answer = 1;
+            return answer;
+        } else {
+            int answer = -1;
+            return answer;
+        }     
+        
     }
 }
-
-//커밋 테스트용 테스트
