1144959d23409aab1d5024e7adfeae87cf4fc6bf
이창환님 <spacecehh@naver.com>
2024-09-09 01:59:32 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        
+        for(int i = 0; i<answer.length; i++){
+            answer[i]=2*i+1;
+        }
+        return answer;
+    }
+}
