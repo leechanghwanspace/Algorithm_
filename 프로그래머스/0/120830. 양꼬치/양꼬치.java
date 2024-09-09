d16890cce073588b27231144130721fd6485ed8a
이창환님 <spacecehh@naver.com>
2024-09-09 22:25:30 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -2,10 +2,10 @@ class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         
-        if(n < 10){
-            answer = n * 12000 + 2000 * k;
+        if( n < 10){
+            answer = n * 12000 + k * 2000;
         } else {
-            answer = n * 12000 + 2000 * k - 2000 * (n / 10);
+            answer = n * 12000 + k * 2000 - (n / 10) * 2000;
         }
         return answer;
     }
