0d3054d85dc32e2afd9ac087369ec47eb5c115be
이창환님 <spacecehh@naver.com>
2024-09-18 00:53:54 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int slice, int n) {
+        int answer = 0;
         
-        int answer = n / slice;
-        
-        if(n % slice > 0){
-            answer += 1;
+        if( n%slice == 0){
+            answer = n/slice;
+        } else{
+            answer = n/slice +1;
         }
         
         return answer;
-
     }
 }
