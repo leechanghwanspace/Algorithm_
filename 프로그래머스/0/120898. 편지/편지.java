ba02192ada01e811be49d2d3fb0ce2835a6ab71c
이창환님 <spacecehh@naver.com>
2024-09-18 00:37:56 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(String message) {
         
-        int length = message.length();
+        int answer = message.length()*2;
         
-        int answer = length*2;
         
         return answer;
     }
